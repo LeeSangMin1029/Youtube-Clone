7fdffebb6799d3d9df835e0ea6a791527aa1b8ba
LeeSangMin1029 <sangmins39@gmail.com>
2021-02-04 00:59:04 +0900

    api를 호출을 위한 설정 값 추가

--- src/config/index.js
+++ src/config/index.js
@@ -10,4 +10,7 @@ if (envFound.error) {
 export default {
   port: parseInt(process.env.PORT, 10),
   __dirname: dirname(new URL(import.meta.url).pathname).substr(1, 29),
+  youtube_api_key: process.env.YOUTUBE_API_KEYS,
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
 };
