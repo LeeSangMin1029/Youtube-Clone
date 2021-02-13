03e7088ab8c34bd69662d2723a3f409e5f6c81de
LeeSangMin1029 <sangmins39@gmail.com>
2021-02-13 22:31:18 +0900

    __dirname을 정해주는 함수 변경

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const envFound = dotenv.config();
 
@@ -9,7 +10,10 @@ if (envFound.error) {
 
 export default {
   port: parseInt(process.env.PORT, 10),
-  __dirname: dirname(new URL(import.meta.url).pathname).substr(1, 29),
+  __dirname: dirname(fileURLToPath(import.meta.url)).replace(
+    'src\\config',
+    'src'
+  ),
   youtube_api_key: process.env.YOUTUBE_API_KEYS,
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
