ff6db2d08bba643862858c03c0b28553985d387f
LeeSangMin1029 <sangmins39@gmail.com>
2021-02-13 22:32:53 +0900

    webpack-dev-middleware 적용

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,16 +1,26 @@
 import express from 'express';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
 import cors from 'cors';
 
-import config from '../config';
+import { joinDir } from '../util';
 import home from '../routes/home';
+
+import webpackConfig from '../../webpack.config';
+const compiler = webpack(webpackConfig);
 
 export default (app) => {
   app.set('view engine', 'pug');
-  app.set('views', `${config.__dirname}/views`);
+  app.set('views', joinDir('views'));
   app.use(cors());
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use('/static', express.static(`${config.__dirname}/public`));
+  app.use('/static', express.static(joinDir('public')));
   // routes
   app.use('/', home);
   /// error handlers
