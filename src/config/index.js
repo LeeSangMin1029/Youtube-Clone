0a0500a7d633d2d33abfe3aeb0ec19807494db2b
LeeSangMin1029 <sangmins39@gmail.com>
2021-02-20 14:44:21 +0900

    env 설정 파일에 있는 값들을 변수에 저장

--- src/config/index.js
+++ src/config/index.js
@@ -3,19 +3,26 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const envFound = dotenv.config();
-
 if (envFound.error) {
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
+const {
+  YOUTUBE_API_KEYS: youtube_api_key = '',
+  CLIENT_ID: client_id = '',
+  CLIENT_SECRET: client_secret = '',
+  COOKIE_KEY: cookie_key = '',
+  PORT: port = 3000,
+} = process.env;
+
 export default {
-  port: parseInt(process.env.PORT, 10),
+  port: parseInt(port, 10),
   __dirname: dirname(fileURLToPath(import.meta.url)).replace(
     'src\\config',
     'src'
   ),
-  youtube_api_key: process.env.YOUTUBE_API_KEYS,
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  cookie_key: process.env.COOKIE_KEY,
+  youtube_api_key,
+  client_id,
+  client_secret,
+  cookie_key,
 };
