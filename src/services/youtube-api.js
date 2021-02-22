265a6ba19831da600a39bedcddd02e3fc1362810
LeeSangMin1029 <sangmins39@gmail.com>
2021-02-23 00:20:44 +0900

    api 기본 세팅
    로그인을 하면 인증하는 URL로 redirect 된 후 root 경로로 돌아온다.

--- src/services/youtube-api.js
+++ src/services/youtube-api.js
@@ -5,18 +5,46 @@ import config from '../config';
 const oauth2Client = new google.auth.OAuth2(
   config.client_id,
   config.client_secret,
-  `${config.base}/google/callback`
+  `${config.base}/auth/google/code`
 );
+
+google.options({
+  auth: oauth2Client,
+});
 
 const scopes = [
   'https://www.googleapis.com/auth/youtube',
   'https://www.googleapis.com/auth/youtube.force-ssl',
   'https://www.googleapis.com/auth/youtube.readonly',
+  'https://www.googleapis.com/auth/youtubepartner',
 ];
 
-const url = oauth2Client.generateAuthUrl({
+let refreshToken = null;
+
+const authorizeURL = oauth2Client.generateAuthUrl({
   access_type: 'offline',
   scope: scopes,
 });
 
-export default { url, oauth2Client };
+const setToken = async (code) => {
+  if (refreshToken !== null) {
+    oauth2Client.setCredentials({
+      refresh_token: refreshToken,
+    });
+    return;
+  }
+  const { tokens } = await oauth2Client.getToken(code);
+  refreshToken = tokens.refresh_token;
+  oauth2Client.credentials = tokens;
+};
+
+const getAPIData = async () => {
+  const service = google.youtube('v3');
+  const res = await service.subscriptions.list({
+    mine: true,
+    part: 'snippet',
+  });
+  return res;
+};
+
+export { authorizeURL, setToken, getAPIData };
