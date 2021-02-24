6b72edf1a2e3c09be2be02dc4b399b8d7fe9895a
LeeSangMin1029 <sangmins39@gmail.com>
2021-02-24 23:56:08 +0900

    서버에서 google api를 요청해서 사용자가 구독한 채널 목록을 불러온다

--- src/controller/home-controller.js
+++ src/controller/home-controller.js
@@ -1,8 +1,26 @@
 import { ash } from '../util';
+import { getAPIData } from '../services/youtube-api';
 
 const renderMain = ash(async (req, res) => {
   try {
-    return res.render('home/main');
+    const {
+      data: { items },
+    } = await getAPIData({
+      auth: oauth2Client,
+      mine: true,
+      part: 'snippet',
+    });
+    const subscribed = items.map((item) => {
+      const {
+        snippet: { channelId, title, thumbnails },
+      } = item;
+      return {
+        href: `/${channelId}`,
+        string: title,
+        imgSrc: thumbnails.default.url,
+      };
+    });
+    return res.render('home/main', { subscribed });
   } catch (err) {
     throw new Error(err);
   }
