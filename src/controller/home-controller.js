e235e89040c0be8b274885b875b48aa8bd1e75cf
LeeSangMin1029 <sangmins39@gmail.com>
2021-03-01 23:16:25 +0900

    유튜브 api 호출

--- src/controller/home-controller.js
+++ src/controller/home-controller.js
@@ -1,7 +1,11 @@
 import { ash } from '../util';
+import { subscriptionHandler } from '../services';
 
+const handler = subscriptionHandler();
 const renderMain = ash(async (req, res) => {
   try {
+    const data = await handler.list({ part: 'snippet', mine: true });
+    console.log(data);
     return res.render('home/main');
   } catch (err) {
     throw new Error(err);
