bba45018c5e34ec26798c42e8d5e5f34bcbceaf9
LeeSangMin1029 <sangmins39@gmail.com>
2021-03-01 16:02:51 +0900

    Update models

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,18 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  profile_id: String,
-  email: String,
-  locale: String,
-  name: String,
-  refresh_token: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    google_id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    image_url: { type: String, required: true },
+    accepted: { type: Date, default: Date.now },
+    locale: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const User = mongoose.model('User', userSchema);
 export default User;
