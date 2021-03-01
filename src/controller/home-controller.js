24e75a20970e612df79fea84701d21c1f3d04cfb
LeeSangMin1029 <sangmins39@gmail.com>
2021-03-01 16:10:58 +0900

    Delete api logic

--- src/controller/home-controller.js
+++ src/controller/home-controller.js
@@ -1,26 +1,8 @@
 import { ash } from '../util';
-import { getAPIData, oauth2Client } from '../services/youtube-api';
 
 const renderMain = ash(async (req, res) => {
   try {
-    const {
-      data: { items },
-    } = await getAPIData({
-      auth: oauth2Client,
-      mine: true,
-      part: 'snippet',
-    });
-    const subscribed = items.map((item) => {
-      const {
-        snippet: { channelId, title, thumbnails },
-      } = item;
-      return {
-        href: `/${channelId}`,
-        string: title,
-        imgSrc: thumbnails.default.url,
-      };
-    });
-    return res.render('home/main', { subscribed });
+    return res.render('home/main');
   } catch (err) {
     throw new Error(err);
   }
