581e0f8f9fc5c28348f304af8fbb9ede1bc9f644
LeeSangMin1029 <sangmins39@gmail.com>
2021-03-03 01:27:14 +0900

    JWT를 생성, 확인하는 미들웨어 새로 추가

--- src/loaders/middlewares.js
+++ src/loaders/middlewares.js
@@ -1,3 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+import config from '../config';
+import { setAuthCredentials } from '../services';
+
 const localsMiddleware = (req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.currentUser = req.user;
@@ -5,4 +10,34 @@ const localsMiddleware = (req, res, next) => {
   next();
 };
 
-export { localsMiddleware };
+const createToken = (req, res, next) => {
+  const { token } = req.user;
+  const jwtToken = jwt.sign({ token }, config.jwt_secret_key, {
+    expiresIn: token.expires_in,
+  });
+  res.cookie('jwt_token', jwtToken);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    const clientToken = req.cookies.jwt_token;
+    const decoded = jwt.verify(clientToken, config.jwt_secret_key);
+    if (decoded !== undefined) {
+      res.token = decoded.token;
+      next();
+    } else {
+      res.status(401).json({ error: 'unauthorized' });
+    }
+  } catch (err) {
+    res.status(401).json({ error: 'token expired' });
+  }
+};
+
+const credentials = (_, res, next) => {
+  console.log(res.token);
+  setAuthCredentials(res.token);
+  next();
+};
+
+export { localsMiddleware, createToken, verifyToken, credentials };
