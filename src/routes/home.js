89a2dab99131e7439ef14a4b07810dac16e90ce8
LeeSangMin1029 <sangmins39@gmail.com>
2021-03-03 01:29:37 +0900

    JWT을 확인하고 API를 호출하기 위한 oauth 설정하는 미들웨어 추가

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
 import * as homeController from '../controller/home-controller';
+import { verifyToken, credentials } from '../loaders/middlewares';
 
 const route = Router();
-route.get('/', homeController.renderMain);
+route.get('/', verifyToken, credentials, homeController.renderMain);
 
 export default route;
